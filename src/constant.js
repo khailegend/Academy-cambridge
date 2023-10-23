ac2166819413cc68bc40a9e69eabd42f5e44a333
Abel Tran <abeltran.develop@gmail.com>
2023-10-23 11:32:53 +0700

    add data

---
+++ src/constant.js
@@ -0,0 +1,171 @@
+export const programs = [
+  {
+    nameVn: "Giáo Dục Phổ Thông",
+    nameEn: "Standard",
+    description: "Chương trình giáo dục phổ thông được thiết kế và phát triển theo đúng chuẩn của bộ giáo dục và đào tạo."
+  },
+  {
+    nameVn: "Quốc Tế Cambridge",
+    nameEn: "Cambridge",
+    description: "Chương trình giáo dục Quốc Tế Cambridge được thiết kế và phát triển theo đúng chuẩn của trường Đại học Cambridge (Top 1 Anh Quốc)."
+  },
+  {
+    nameVn: "Chương Trình Tích Hợp",
+    nameEn: "Integration",
+    description: "Chương trình giáo dục Tích Hợp được thiết kế và phát triển theo đúng chuẩn của trường Đại học Cambridge (Top 1 Anh Quốc) và bộ giáo dục đào tạo.",
+  },
+  {
+    nameVn: "Luyện Thi Chứng Chỉ",
+    nameEn: "Test preparation For Certificate",
+    description: "Luyện thi chứng chỉ hỗ trợ cho các bé về tài liệu cũng như các phương pháp mà các bé sẽ làm trong kì thi"
+  },
+  {
+    nameVn: "Luyện Thi Trường Giỏi",
+    nameEn: "Test preparation For Top School",
+    description: "Luyện thi trường giỏi hỗ trợ cho các bé về tài liệu cũng như các phương pháp mà các bé sẽ làm trong kì thi"
+  }
+]
+
+export const certificates = [
+  {
+    shortCode: "STARTERS",
+    nameEn: "Pre A1 Starters",
+    description: "Pre A1 Starters' là một trong những chứng chỉ thuộc Cambridge English Qualifications. Đây là sự khởi đầu cho hành trình học ngôn ngữ của trẻ em. Pre A1 Starters là bài thi đầu tiên trong bộ ba bài thi của chương trình 'Cambridge English: Young Learners (YLE)' dành cho trẻ em.",
+  }, {
+    shortCode: "MOVERS",
+    nameEn: "A1 Movers",
+    description: "A1 Movers' là một trong những chứng chỉ thuộc Cambridge English Qualifications. Đây là sự khởi đầu cho hành trình học ngôn ngữ của trẻ em. Pre A1 Starters là bài thi đầu tiên trong bộ ba bài thi của chương trình 'Cambridge English: Young Learners (YLE)' dành cho trẻ em.",
+
+  }, {
+    shortCode: "FLYERS",
+    nameEn: "A2 Flyers",
+    description: "A2 Flyers' là một trong những chứng chỉ thuộc Cambridge English Qualifications. Đây là bài thi thứ ba trong số các bài thi kiểm tra thực hành tiếng Anh vui nhộn của chúng tôi dành cho trẻ em.",
+
+  }, {
+    shortCode: "KET",
+    nameEn: "Key English Test",
+    description: "Key English Test là chứng chỉ Tiếng Anh trình độ sơ cấp (A2), do hội đồng khảo thí Tiếng Anh đại học Cambridge (Cambridge ESOL), Vương quốc Anh cấp.",
+
+  }, {
+    shortCode: "PET",
+    nameEn: "Preliminary English Test",
+    description: "Preliminary English Test là chứng chỉ Tiếng Anh trình độ trung sơ cấp (B1), do hội đồng khảo thí Tiếng Anh đại học Cambridge (Cambridge ESOL), Vương quốc Anh cấp.",
+
+  }, {
+    shortCode: "FCE",
+    nameEn: "First Certificate in English",
+    description: "First Certificate in English là chứng chỉ Tiếng Anh trình độ trung cấp (B2), do hội đồng khảo thí Tiếng Anh đại học Cambridge (Cambridge ESOL), Vương quốc Anh cấp.",
+
+  }, {
+    shortCode: "IELTS",
+    nameEn: "International English Language Testing System",
+    description: "International English Language Testing System là một hệ thống bài kiểm tra về khả năng sử dụng thành thạo tiếng Anh trải dài qua cả bốn kĩ năng Nghe, Nói, Đọc, Viết. Được đồng điều hành bởi ba tổ chức ESOL của Đại học Cambridge (University of Cambridge ESOL), Hội đồng Anh (British Council) và tổ chức giáo dục IDP của Úc.",
+
+  }, {
+    shortCode: "SAT",
+    nameEn: "Scholastic Assessment Test or Scholastic Aptitude Test",
+    description: "Scholastic Assessment Test hay Scholastic Aptitude Test là bài kiểm tra đánh giá năng lực chuẩn hóa được sử dụng rộng rãi cho xét tuyển đại học trong hệ thống giáo dục Mỹ.",
+
+  }, {
+    shortCode: "A Level",
+    nameEn: "General Certificate of Education Advanced Level",
+    description: "GCEA level là một chứng chỉ được cấp bởi các trường tại Anh, Xứ Wales, và Bắc Ireland cho học sinh khoảng 16 - 19 tuổi trước khi thi vào đại học. A level được xem như tiêu chuẩn vàng, được sử dụng bởi các trường đại học tại Anh nói riêng và quốc tế nói chung trong việc tìm kiếm và lọc ứng viên sáng giá.",
+
+  }, {
+    shortCode: "AP",
+    nameEn: "Advanced Placement",
+    description: "Advanced Placement (AP) là chương trình giáo dục dùng để đáng giá xét tuyển đại học và ngày càng phổ biến ở Mỹ.",
+
+  }, {
+    shortCode: "IB",
+    nameEn: "International Baccalaureate",
+    description: "International Baccalaureate (tú tài quốc tế) là chương trình giáo dục phi lợi nhuận được thành lập vào năm 1968 và chú trọng phát triển vào 4 khía cạnh. Đó là trí tuệ, cá nhân, cảm xúc và các kỹ năng mềm nhằm phát triển khả năng học tập cũng như làm việc trong môi trường toàn cầu hóa hiện nay.",
+
+  }, {
+    shortCode: "IGCSE",
+    nameEn: "The International General Certificate of Secondary Education",
+    description: "IGCSEs là chứng chỉ giáo dục Trung học phổ thông phổ biến trên thế giới dành cho học sinh từ 14 -16 tuổi.",
+
+  }, {
+    shortCode: "Cambridge Primary Checkpoint",
+    nameEn: "Cambridge Primary Checkpoint",
+    description: "Cambridge Primary Checkpoint là chứng chỉ quốc tế do Đại học Cambridge, Anh Quốc cấp cho các học sinh cấp tiểu học trên toàn thế giới từ 5 đến 11 tuổi. Chứng chỉ này được thiết kế để đánh giá năng lực tiếng Anh của học sinh trong ba môn học chính: Tiếng Anh, Toán học và Khoa học.",
+
+  }, {
+    shortCode: "Cambridge Lower Secondary Checkpoint",
+    nameEn: "Cambridge Lower Secondary Checkpoint",
+    description: "Cambridge Lower Secondary Checkpoint là chứng chỉ quốc tế do Đại học Cambridge, Anh Quốc cấp cho các học sinh cấp tiểu học trên toàn thế giới từ 11 đến 14 tuổi. Chứng chỉ này được thiết kế để đánh giá năng lực tiếng Anh của học sinh trong ba môn học chính: Tiếng Anh, Toán học và Khoa học.",
+
+  },];
+
+export const schools = [
+  {
+    nameVn: "Trường Quốc Tế Vinschool",
+    nameEn: "Vin International School",
+    code: "VS"
+  },
+  {
+    nameVn: "Trường Quốc Tế Anh",
+    nameEn: "British International School",
+    code: "BIS"
+  },
+  {
+    nameVn: "Trường Quốc Tế Anh Việt",
+    nameEn: "British Vietnamese International School",
+    code: "BVIS"
+  },
+  {
+    nameVn: "Trường Phổ thông Song ngữ Quốc tế Wellspring",
+    nameEn: "Wellspring International Bilingual School",
+    code: "Wellspring"
+  },
+  {
+    nameVn: "Trường Quốc Tế eSchool",
+    nameEn: "A Boutique Village School",
+    code: "eSchool"
+  },
+];
+
+export const subjects = [
+  {
+    nameVn: "Toán",
+    nameEn: "Math",
+    description: "Toán học là môn khoa học nghiên cứu về các số, cấu trúc, không gian và các phép biến đổi. Nói một cách khác, người ta cho rằng đó là môn học về hình và số"
+  },
+  {
+    nameVn: "Tiếng Anh",
+    nameEn: "English",
+    description: "Tiếng Anh là môn khoa học ngôn ngữ (ngôn ngữ Giécmanh Tây thuộc ngữ hệ Ấn-Âu)."
+  },
+  {
+    nameVn: "Khoa học Tự Nhiên",
+    nameEn: "Science",
+    description: "Khoa học Tự Nhiên là môn khoa học giúp trẻ phát triển logic cơ bản và tư duy phân tích vấn đề."
+  },
+  {
+    nameVn: "Kinh Doanh",
+    nameEn: "Business",
+    description: "Kinh Doanh là môn lĩnh vực nghiên cứu liên quan đến các nguyên tắc kinh doanh, quản lý và kinh tế. Nó kết hợp các yếu tố kế toán, tài chính, tiếp thị, nghiên cứu tổ chức, quản lý nguồn nhân lực và hoạt động."
+  },
+  {
+    nameVn: "Kinh tế học",
+    nameEn: "Economics",
+    description: "Kinh tế học là một ngành khoa học xã hội nghiên cứu việc sản xuất, phân phối và tiêu thụ hàng hóa và dịch vụ."
+  },
+  {
+    nameVn: "Sinh học",
+    nameEn: "Biology",
+    description: "Sinh học là một môn khoa học nghiên cứu về thế giới sinh vật.Nó là một nhánh của khoa học tự nhiên, có phạm vi rộng nhưng có một số chủ đề thống nhất gắn kết nó với nhau thành một lĩnh vực duy nhất, mạch lạc."
+  },
+  {
+    nameVn: "Hóa học",
+    nameEn: "Chemistry",
+    description: "Hóa học (gọi tắt là hóa) là một nhánh của khoa học tự nhiên nhằm nghiên cứu về thành phần, cấu trúc, tính chất, và sự thay đổi của vật chất."
+  },
+  {
+    nameVn: "Vật lý",
+    nameEn: "Physics",
+    description: "Vật lý là một môn khoa học tự nhiên tập trung vào sự nghiên cứu vật chất[1] và chuyển động của nó trong không gian và thời gian, cùng với những khái niệm liên quan như năng lượng và lực."
+  }
+
+]
